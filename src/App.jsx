73877226e7b636413f4453aa745d1923a5db8169
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-19 01:48:56 -0300

    feat: add function to reset Imc calculator

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,13 @@ function App() {
     if (!info) return;
   };
 
+  const resetCalc = (e) => {
+    e.preventDefault();
+    setImc("");
+    setInfo("");
+    setInfoClass("");
+  };
+
   const [imc, setImc] = useState("");
   const [info, setInfo] = useState("");
   const [infoClass, setInfoClass] = useState("");
@@ -38,7 +45,13 @@ function App() {
       {!imc ? (
         <ImcCalc calcImc={calcImc} />
       ) : (
-        <ImcTable data={data} imc={imc} info={info} infoClass={infoClass} />
+        <ImcTable
+          data={data}
+          imc={imc}
+          info={info}
+          infoClass={infoClass}
+          resetCalc={resetCalc}
+        />
       )}
     </div>
   );
