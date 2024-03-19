aaba7de45feae7b13f4236579a64ffb79ce45b9c
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-19 01:08:41 -0300

    feat: add function to calc Imc

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { data } from "./data/data";
+
+import { useState } from "react";
+
 import ImcCalc from "./components/ImcCalc";
+import ImcTable from "./components/ImcTable";
 
 import "./App.css";
 
 function App() {
+  const calcImc = (e, height, weight) => {
+    e.preventDefault();
+
+    if (!height || !weight) return;
+
+    const heightFloat = +height.replace(",", ".");
+    const weightFloat = +weight.replace(",", ".");
+    const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1);
+
+    setImc(imcResult);
+  };
+
+  const [imc, setImc] = useState("");
+  const [info, setInfo] = useState("");
+  const [infoClass, setInfoClass] = useState("");
+
   return (
     <div className="container">
-      <ImcCalc />
+      {!imc ? <ImcCalc calcImc={calcImc} /> : <ImcTable />}
     </div>
   );
 }
--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Button from "./Button";
 import "./ImcCalc.css";
 
-const ImcCalc = () => {
+const ImcCalc = ({ calcImc }) => {
   const [height, setHeight] = useState("");
   const [weight, setWeight] = useState("");
 
@@ -47,7 +47,11 @@ const ImcCalc = () => {
           </div>
         </div>
         <div className="action-control">
-          <Button id="calc-btn" text="Calcular" />
+          <Button
+            id="calc-btn"
+            text="Calcular"
+            action={(e) => calcImc(e, height, weight)}
+          />
           <Button id="clear-btn" text="Limpar" action={clearForm} />
         </div>
       </form>
